b946589873fe142f1d7ae488db198b3975d4f61a
juanlavallen <juanlavallen02@gmail.com>
2022-05-08 13:22:47 -0300

    Inputs

--- Variables/variables.py
+++ Variables/variables.py
@@ -11,3 +11,13 @@ print(f'Age: {age}')
 # full_name = first_name + ' ' + last_name
 full_name = f'{first_name} {last_name}'
 print(full_name)
+
+### Inputs ###
+
+color = input('Ingrese un color')
+country = input('Ingrese un Pais')
+car = input('Ingrese una marca de Autos')
+
+print(f'El Color ingresado es: {color}')
+print(f'El Pais  ingresado es: {country}')
+print(f'La marca ingresada es: {car}')
