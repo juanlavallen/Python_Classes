70cc1d3a5078daa4e2096cb79ea24369b4ef0a4e
juanlavallen <juanlavallen02@gmail.com>
2022-05-08 13:20:36 -0300

    Variables

---
+++ Variables/variables.py
@@ -0,0 +1,13 @@
+# Variables
+
+first_name = 'Juan'
+last_name = 'Lavallen'
+age = 19
+
+print(f'First Name: {first_name}')
+print(f'Last  Name: {last_name}')
+print(f'Age: {age}')
+
+# full_name = first_name + ' ' + last_name
+full_name = f'{first_name} {last_name}'
+print(full_name)
