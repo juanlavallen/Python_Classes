295d09f9b3af9e93b1e517bf115de8838835d8fa
juanlavallen <juanlavallen02@gmail.com>
2022-05-13 00:24:02 -0300

    creando un nuevo elemento

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -11,3 +11,7 @@ print(data['sons'])
 # Cambiando el valor de 'name'
 data['name'] = 'Lionel Messi'
 print(data['name'])  # --> Lionel Messi
+
+# Creando una nueva clave con un nuevo valor
+data['nationality'] = 'Argetina'
+print(data)
