642e93a8c9559a2928adf60b1df1917e1d27076d
juanlavallen <juanlavallen02@gmail.com>
2022-05-13 13:52:36 -0300

    class person

---
+++ Classes/classes.py
@@ -0,0 +1,22 @@
+### Classes ###
+
+class Person:
+    def __init__(self, name, age, nationality) -> None:  # constructor
+        self.name = name
+        self.age = age
+        self. nationality = nationality
+
+    def write(self):
+        data = f'''
+        Name: {self.name}
+        Age: {self.age}
+        Nationality: {self.nationality}
+        '''
+        print(data)
+
+    def __str__(self) -> str:
+        return f'Person: {self.name}, {self.age}, {self.nationality}'
+
+
+person = Person('Lionel Messi', 34, 'Argentina')  # Instancia de la clase
+print(person)
