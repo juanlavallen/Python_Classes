18cb48c04176c0c6ac7eb9f122eaf35ffdfeb6dd
juanlavallen <juanlavallen02@gmail.com>
2022-05-17 17:20:40 -0300

    args

--- Functions/lambdas.py
+++ Functions/lambdas.py
@@ -6,3 +6,13 @@ def add(a, b): return a + b
 #function lambda
 add = lambda a, b: a + b
 print(add(10, 20))
+
+### args - kwargs ###
+
+def add_args(*args):
+    total = 0
+    for n in args:
+        total +=  n
+    return print('Total:', total) 
+    
+add_args(1, 2, 3, 4, 5)
