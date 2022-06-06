476f36ab4a92c9f1277670118447735ac73de1da
juanlavallen <juanlavallen02@gmail.com>
2022-06-06 18:19:13 -0300

    Comenzando ejercicio de POO

---
+++ Classes/Example/Person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name, age, nationality, gender) -> None:
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+        self.gender = gender
