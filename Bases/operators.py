7b5937ff5a4c99cd3a1218fff089b6fed3fe237c
Juan Lavallen <juanlavallen02@gmail.com>
2022-07-02 14:34:25 -0300

    operators

---
+++ Bases/operators.py
@@ -0,0 +1,18 @@
+# Operators
+
+a = 2
+b = 3
+
+mayor = a > b
+menor = a < b
+mayor_igual = a >= b
+menor_igual = a <= b
+igual = a == b
+diferente = a != b
+
+print(mayor)
+print(mayor)
+print(mayor_igual)
+print(menor_igual)
+print(igual)
+print(diferente)
