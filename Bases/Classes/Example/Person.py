915a7956b397ba3e28c46f7d2572a8b73757a45d
Juan Lavallen <juanlavallen02@gmail.com>
2022-07-02 14:29:08 -0300

    deleted folders

--- Bases/Classes/Example/Person.py
+++
@@ -1,6 +0,0 @@
-class Person:
-    def __init__(self, name, age, nationality, gender) -> None:
-        self.name = name
-        self.age = age
-        self.nationality = nationality
-        self.gender = gender
