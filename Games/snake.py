d81d65654c96cfcc5dde133304f660a0d845fa0e
juanlavallen <juanlavallen02@gmail.com>
2022-07-01 23:24:59 -0300

    head settings

--- Games/snake.py
+++ Games/snake.py
@@ -7,4 +7,14 @@ window.title("Snake Game")
 window.bgcolor("black")
 window.setup(width=600, height=600)
 
+# Head Settings
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "up"
+
+
 turtle.done()
