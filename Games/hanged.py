79a9d72a8de57310e7b868187b834e129895db93
Juan Lavallen <juanlavallen02@gmail.com>
2022-07-01 22:18:47 -0300

    hanged game

---
+++ Games/hanged.py
@@ -0,0 +1,99 @@
+import os
+import random
+
+
+def run():
+    IMAGES = ['''
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|   |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+        /    |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+        / \  |
+            |
+        ========='''
+              ]
+
+    DB = [
+        "C#",
+        "PYTHON",
+        "JAVASCRIPT",
+        "PHP"
+    ]
+
+    word = random.choice(DB)
+    spaces = ["_"] * len(word)
+    attemps = 6
+
+    while True:
+        os.system("cls")
+        for character in spaces:
+            print(character, end=" ")
+        print(IMAGES[attemps])
+        letter = input("Elegi una letra ").upper()
+
+        found = False
+        for idx, character in enumerate(word):
+            if character == letter:
+                spaces[idx] = letter
+                found = True
+
+        if not found:
+            attemps -= 1
+
+        if "_" not in spaces:
+            os.system("cls")
+            print("Ganaste")
+            break
+            input()
+
+        if attemps == 0:
+            os.system("cls")
+            print("Perdiste")
+            break
+            input()
+
+
+if __name__ == '__main__':
+    run()
