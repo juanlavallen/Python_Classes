e9ee894085f68aa2cfe804c718f9c36b1129bc90
juanlavallen <juanlavallen02@gmail.com>
2022-07-08 19:27:28 -0300

    tuples files added

---
+++ Bases/tuples.py
@@ -0,0 +1,6 @@
+numbers_tuple = (1, 2, 3, 4, 5)
+print(numbers_tuple[0:3])  # 0 a 3 => 1, 2, 3
+
+string_tuple = ('A', 'B', 'C', 'D', 'E', 'F', 'A')
+print(string_tuple.count('A'))
+print(string_tuple.index('A')) # Agarra la 1er posicion que contiene 'A'
