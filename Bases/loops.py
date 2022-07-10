00514cab5547cba721fb5410016aa1318631426d
appcresi <appcresi@gmail.com>
2022-07-10 19:13:52 -0300

    for in dictionary

--- Bases/loops.py
+++ Bases/loops.py
@@ -9,5 +9,15 @@ while quantity <= 5:
 ### For ###
 users = ['juan02_devjr', 'raul_SQL', 'lucia_UX']
 
-for n in users:
-    print(n)
+for user in users:
+    print(user)
+
+dictionary = {
+    'key_1': 1,
+    'key_2': 2,
+    'key_3': 3,
+    'key_4': 4
+}
+
+for key, value in dictionary.items():
+    print(value)
