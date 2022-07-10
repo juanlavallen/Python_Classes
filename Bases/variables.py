3b6101102451457cbf5601a514bf95562d2415a8
juanlavallen <juanlavallen02@gmail.com>
2022-07-10 19:25:33 -0300

    tab

--- Bases/variables.py
+++ Bases/variables.py
@@ -26,9 +26,9 @@ print(full_name)
 
 ### Inputs ###
 
-color = input('Ingrese un color')
+color   = input('Ingrese un color')
 country = input('Ingrese un Pais')
-car = input('Ingrese una marca de Autos')
+car     = input('Ingrese una marca de Autos')
 
 print(f'El Color ingresado es: {color}')
 print(f'El Pais  ingresado es: {country}')
