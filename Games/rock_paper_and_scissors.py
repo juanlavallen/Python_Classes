388c6c0178e45671b492f4c1ce56144b795c937e
Juan Lavallen <juanlavallen02@gmail.com>
2022-07-13 21:17:30 -0300

    game finished

--- Games/rock_paper_and_scissors.py
+++ Games/rock_paper_and_scissors.py
@@ -1,5 +1,20 @@
+import random
+
+
 def play():
-    print('Rock - Paper - Scissors')
+    print('Piedra: PI - Papel: PA - Tijera: TI')
+
+    user = input('Ingresa una opcion: ').upper()
+    computer = random.choice(['PI', 'PA', 'TI'])
+
+    if ((user == 'PI' and computer == 'TI')
+        or (user == 'TI' and computer == 'PA')
+        or (user == 'PA' and computer == 'PI')):
+        return print('¡Ganaste!')
+    elif user == computer:
+        return print('¡Empate!')
+    else:
+        return print('¡Perdiste!')
 
 
 if __name__ == '__main__':
