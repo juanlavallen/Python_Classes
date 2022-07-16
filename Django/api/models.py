579fc5d9b0e89f58a27a5742b7a85f44be48cf74
juanlavallen <juanlavallen02@gmail.com>
2022-07-15 21:27:09 -0300

    model user created

--- Django/api/models.py
+++ Django/api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, name, email, password=None):
+        if not email:
+            raise ValueError('El usuario debe tener un Email')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, name, email, password):
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects: UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    def get_full_name(self):
+        return self.name
+
+    def __str__(self) -> str:
+        return self.email
